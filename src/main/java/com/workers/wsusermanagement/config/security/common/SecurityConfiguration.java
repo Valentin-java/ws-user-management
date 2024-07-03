3854b4465570cb65e9c07077ebca7ae7c9aabbb1
Казанцев Валентин Игоревич <VIKazantsev@alfabank.ru>
2024-07-03 11:18:57 +0300

    WRKS-0005: Some fix

--- src/main/java/com/workers/wsusermanagement/config/security/common/SecurityConfiguration.java
+++ src/main/java/com/workers/wsusermanagement/config/security/common/SecurityConfiguration.java
@@ -1,5 +1,6 @@
-package com.workers.wsusermanagement.config.security;
+package com.workers.wsusermanagement.config.security.common;
 
+import com.workers.wsusermanagement.config.security.filter.TokenAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/workers/wsusermanagement/config/security/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/workers/wsusermanagement/config/security/filter/TokenAuthenticationFilter.java
@@ -1,8 +1,7 @@
-package com.workers.wsusermanagement.config.security;
+package com.workers.wsusermanagement.config.security.filter;
 
 import com.workers.wsusermanagement.config.security.context.TokenAuthenticationFilterContext;
-import com.workers.wsusermanagement.util.SecurityValidationUtil;
-import io.jsonwebtoken.Claims;
+import com.workers.wsusermanagement.config.security.util.SecurityValidationUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,24 +9,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
-import static com.workers.wsusermanagement.util.Constants.AUTH_TOKEN_PREFIX;
-import static com.workers.wsusermanagement.util.Constants.UNEXPECTED_TEXT_ERROR;
-import static java.util.stream.Collectors.toList;
+import static com.workers.wsusermanagement.config.security.util.Constants.UNEXPECTED_TEXT_ERROR;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Slf4j
@@ -64,30 +56,22 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private TokenAuthenticationFilterContext validateHeader(TokenAuthenticationFilterContext context) {
-        String header = securityUtil.getHeaderRequest(context);
-        if (Strings.isEmpty(header)
-                || !header.startsWith(AUTH_TOKEN_PREFIX)) {
+        if (securityUtil.whenHeaderMissing(context)) {
             throw new ResponseStatusException(UNAUTHORIZED, "Отсуствует, либо заголовок, либо токен");
         }
         return context;
     }
 
     private TokenAuthenticationFilterContext tokenContainUsername(TokenAuthenticationFilterContext context) {
-        String user = securityUtil.getUsername(context);
-
-        if (user == null) {
+        if (securityUtil.whenUsernameMissing(context)) {
             log.error("Token provided but not content data!");
             throw new ResponseStatusException(UNAUTHORIZED, "В токене отсуствует имя пользователя");
         }
         return context;
     }
 
-    // Валидация токена на время жизни
     private TokenAuthenticationFilterContext validateTokenExpiration(TokenAuthenticationFilterContext context) {
-        String token = securityUtil.getToken(context);
-        Claims claims = securityUtil.extractAllClaims(token);
-
-        if (securityUtil.isTokenExpired(claims)) {
+        if (securityUtil.whenTokenExpired(context)) {
             log.error("[validateTokenExpiration] Время жизни токена истекло.");
             throw new ResponseStatusException(UNAUTHORIZED, "Время жизни токена истекло");
         }
@@ -96,23 +80,14 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
     private TokenAuthenticationFilterContext setAuthenticationContext(TokenAuthenticationFilterContext context) {
         String username = securityUtil.getUsername(context);
-        log.debug("[setAuthenticationContext] UserId: {}", username);
+        log.debug("[setAuthenticationContext] username: {}", username);
 
-        var grantedAuthorities = getGrantedAuthority(context);
+        var grantedAuthorities = securityUtil.getGrantedAuthority(context);
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, grantedAuthorities);
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return context;
-    }
-
-    private List<GrantedAuthority> getGrantedAuthority(TokenAuthenticationFilterContext context) {
-        Claims claims = securityUtil.extractAllClaims(securityUtil.getToken(context));
-        String roles = (String) claims.get("roles");
-
-        return Arrays.stream(roles.split(","))
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-                .collect(toList());
     }
 
     private void continueFilterChain(TokenAuthenticationFilterContext context) {
